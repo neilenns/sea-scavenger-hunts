27750a0547aa4b094b9ffe15aea916023f062da5
Neil Enns <info@neilenns.com>
2025-08-20 00:03:14 +0000

    Add a German locale Fixes #53

--- src/components/language-switcher.tsx
+++ src/components/language-switcher.tsx
@@ -7,7 +7,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { usePathname, useRouter } from "@/i18n/navigation";
-import { FlagComponent, FR, US } from "country-flag-icons/react/3x2";
+import { DE, FlagComponent, FR, US } from "country-flag-icons/react/3x2";
 import { GlobeIcon } from "lucide-react";
 import type { Locale } from "next-intl";
 import { useLocale, useTranslations } from "next-intl";
@@ -17,6 +17,7 @@ import { useCallback, useMemo, useTransition } from "react";
 const languages = [
   { code: "en", flagComponent: US },
   { code: "fr", flagComponent: FR },
+  { code: "de", flagComponent: DE },
 ] as const satisfies ReadonlyArray<
   Readonly<{ code: Locale; flagComponent: FlagComponent }>
 >;
