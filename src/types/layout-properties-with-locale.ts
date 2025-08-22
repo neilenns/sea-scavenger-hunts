f45e39fd71b51fd019f5c6f2deaa2c53e67956fc
Neil Enns <info@neilenns.com>
2025-08-22 15:03:32 -0700

    Use new nextjs types (#76)

--- src/types/layout-properties-with-locale.ts
+++
@@ -1,7 +0,0 @@
-import type { Locale } from "next-intl";
-import type { ReactNode } from "react";
-
-export type LayoutPropertiesWithLocale = Readonly<{
-  children: ReactNode;
-  params: Promise<{ locale: Locale }>;
-}>;
